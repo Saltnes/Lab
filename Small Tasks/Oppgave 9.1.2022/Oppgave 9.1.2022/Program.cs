3defdf95be3b02686840ac4fe25fc4dcb2f82330
Saltnes <79427083+Saltnes@users.noreply.github.com>
2022-09-01 10:50:06 +0200

    Add files via upload

---
+++ Small Tasks/Oppgave 9.1.2022/Oppgave 9.1.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oppgave_9._1._2022
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Tast inn en temperatur, et heltall");
+            int temperatur= int.Parse(Console.ReadLine());
+
+            if (temperatur > 0)
+            {
+                Console.WriteLine("varmegrader");
+            }
+            else
+            {
+                Console.WriteLine("minusgrader");
+            }
+
+        }
+    }
+}
