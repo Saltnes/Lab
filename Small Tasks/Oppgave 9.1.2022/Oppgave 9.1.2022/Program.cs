8655ee2467c4038e85b60d6fccabbe017a448a7e
Saltnes <79427083+Saltnes@users.noreply.github.com>
2022-09-01 11:11:53 +0200

    Add files via upload

---
+++ Small Tasks/Oppgave 9.1.2022/Oppgave 9.1.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oppgave_9._1._2022
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Tast inn en temperatur, et heltall"); // Tell user to input int
+            int temperatur= int.Parse(Console.ReadLine()); // Set temperatur variable to user input
+
+            if (temperatur < 0) 
+            {
+                Console.WriteLine("minusgrader");
+            }
+            else
+            {
+                Console.WriteLine("varmegrader");
+            }
+
+        }
+    }
+}
