0041e06777e937e58383285e403524e0bb343b40
Saltnes <79427083+Saltnes@users.noreply.github.com>
2022-09-05 11:14:59 +0200

    Add files via upload
    Tasks 05.09.2022

---
+++ Small Tasks/9.05.2022/LabFindBob/LabFindBob/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFindBob
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Oppgave
+            //lag en metode som finner ut hvor mange bob's det er i stringen under.
+            string bob = "obobobobobobobbbooobboboobobobobobobooobbboobooboboboobboodfdfdob4bobbobooob";
+            
+            Console.WriteLine("Fant: " + Check.CheckOccurrences(bob, "bob") + " Bobs");
+        }
+    }
+
+    public static class Check
+    {
+        public static int CheckOccurrences(string str1, string pattern)
+        {
+            int count = 0;
+            int a = 0;
+            while ((a = str1.IndexOf(pattern, a)) != -1)
+            {
+                a += pattern.Length;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
