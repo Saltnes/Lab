1039ecddb09701044adb342cd063c8527fcb68ff
Saltnes <robinbegb@gmail.com>
2022-09-05 13:02:28 +0200

    commit

--- Small Tasks/9.05.2022/LabFindBob/LabFindBob/Program.cs
+++ Small Tasks/9.05.2022/LabFindBob/LabFindBob/Program.cs
@@ -12,9 +12,24 @@ namespace LabFindBob
         {
             //Oppgave
             //lag en metode som finner ut hvor mange bob's det er i stringen under.
-            string bob = "obobobobobobobbbooobboboobobobobobobooobbboobooboboboobboodfdfdob4bobbobooob";
+            string bob = "obobobobobobobbbooobobobbobobobobobooobbboobooboboboobboodfdfdob4bobbobooob";
+
+           //hvor mange bobs vil bli talt her: bobob. Om det er 2, fix, så det blir 1.
             
             Console.WriteLine("Fant: " + Check.CheckOccurrences(bob, "bob") + " Bobs");
+
+            int counter2 = 0;
+            char[] chars = bob.ToCharArray();
+            for (int i = 0; i < chars.Length-2; i++)
+            {
+                if (chars[i] == 'b' && chars[i + 1] == 'o' && chars[i + 2] == 'b')
+                {
+                    counter2++;//denne teller også halvbobber. hvordan unngå dette?
+                    i = i + 2;
+                }
+            }
+            Console.WriteLine(counter2);
+
         }
     }
 
