629075325909244a5f02b1fa86f8fd31aba6ec7d
Saltnes <79427083+Saltnes@users.noreply.github.com>
2022-10-24 11:16:41 +0200

    Add files via upload

---
+++ Small Tasks/24.10.2022/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (CheckPrime(num))
+                Console.WriteLine(num + " is a prime number");
+            else
+                Console.Write(num + " is not a prime number");
+        }
+
+        private static bool CheckPrime(int number)
+        {
+            for(int i=2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
